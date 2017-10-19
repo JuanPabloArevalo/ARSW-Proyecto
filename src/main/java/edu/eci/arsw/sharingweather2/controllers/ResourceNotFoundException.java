19c531b349d20e936dd16e3b75650cab8d42e1d6
Juan Pablo Arevalo <juan.arevalo.merchan@hotmail.com>
2017-10-19 00:14:40 -0500

    Juan Pablo

--- src/main/java/edu/eci/arsw/sharingweather2/controllers/ResourceNotFoundException.java
+++ src/main/java/edu/eci/arsw/sharingweather2/controllers/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package edu.eci.arsw.sharingWeather.controllers;
+package edu.eci.arsw.sharingweather2.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
--- src/main/java/edu/eci/arsw/sharingweather2/model/Usuario.java
+++ src/main/java/edu/eci/arsw/sharingweather2/model/Usuario.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.eci.arsw.sharingWeather.model;
+package edu.eci.arsw.sharingweather2.model;
 
 /**
  *
