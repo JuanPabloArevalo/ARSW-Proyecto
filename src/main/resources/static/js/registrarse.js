462ee5557215518a15f4bac8f0872dc3effec5d7
stefany288 <stefamoron25@gmail.com>
2017-10-20 13:18:29 -0500

    Stefany

---
+++ src/main/resources/static/js/registrarse.js
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var registrarse = (function(){
+    
+    var nombre;
+    var edad;
+    var nombreUsuario;
+    var contraseña;
+    var correoElectronico;
+    
+    return {
+        
+        
+        
+    };
+}());
+
