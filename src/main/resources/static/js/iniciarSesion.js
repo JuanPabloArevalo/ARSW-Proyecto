a1c3fd5f27835e18b6beae55f3f2adb49d9e7536
stefany288 <stefamoron25@gmail.com>
2017-10-24 18:00:01 -0500

    Stefany

--- src/main/resources/static/js/iniciarSesion.js
+++ src/main/resources/static/js/iniciarSesion.js
@@ -16,7 +16,38 @@ var iniciarSesion = (function(){
             iniciar(){
                 alert("Entro");
                 console.info("entro");
-            }      
+             var error = "";
+             var activarBotonRegistrar = true;
+             
+             nombreUsuario = $("#nombreInicioSesion").val();
+             if(nombreUsuario === ""){
+                error = error + "NombreUsuario.";
+                activarBotonRegistrar = false;
+             }
+             password = $("#contraseñaInicioSesion").val();
+              if(password === ""){
+                error = error + "Contraseña.";
+                activarBotonRegistrar = false;
+              }
+              if(activarBotonRegistrar===false){
+                $("#mensajeFalta").text(error); 
+                $("#divError").show();
+                
+              }else{
+                $("#mensajeFalta").text(""); 
+                $("#divError").hide();
+                $.when(apiclientInicioSesion.autenticacion(nombreUsuario,password )).done(function (usuario){
+                    sessionStorage.setItem("nombreUsuario", usuario.nombreUsuario);
+                    sessionStorage.setItem("password", usuario.password);
+                    //alert("Bienvenido, " + data.name);
+                    window.location.href = "reportarClima.html";
+                }).fail(function(errorThrown){
+                console.log(errorThrown);
+                alert("Usted no se ha autenticado correctamente.");
+                });
+            
+                }
+            }   
     };
     
 });
