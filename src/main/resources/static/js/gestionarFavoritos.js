85442c057e993f8d26c8969833b854ecc4231dc0
stefany288 <stefamoron25@gmail.com>
2017-12-03 20:04:24 -0500

    Stefany

---
+++ src/main/resources/static/js/gestionarFavoritos.js
@@ -0,0 +1,24 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var gestionarFavoritos = (function(){
+    
+    var numero;
+    var nombre;
+    
+    return {
+        
+          addMisFavoritos(){
+              var markup = "<tr class=\"filas\"><td>" + numero + "</td><td>" + nombre + "</td><td><button type=\"button\" class=\"btn btn-success\" onclick=\"app.actualizarPlano('"+numero+"')\">Open</button></td></tr>";
+              $("table tbody").append(markup);
+              
+          }
+        
+        
+    };
+    
+}());
