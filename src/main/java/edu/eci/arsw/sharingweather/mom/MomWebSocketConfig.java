62cf8e574854ed185bf7b1a27dfc10197c68354c
Administrador <Administrador@DESARROLLO1.mshome.net>
2017-12-05 14:52:18 -0500

    Juan Pablo Arevalo. Cambio a CloudAMQP

--- src/main/java/edu/eci/arsw/sharingweather/mom/MomWebSocketConfig.java
+++ src/main/java/edu/eci/arsw/sharingweather/mom/MomWebSocketConfig.java
@@ -5,6 +5,7 @@
  */
 package edu.eci.arsw.sharingweather.mom;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
@@ -18,16 +19,25 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @Configuration
 @EnableWebSocketMessageBroker
 public class MomWebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
-
+    @Value("${host}")
+    private String host;
+    
+    @Value("${puerto}")
+    private int puerto;
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
+        config.enableStompBrokerRelay("/topic/").setRelayHost(host).setRelayPort(puerto).
+                setClientLogin("saueihsf").
+                setClientPasscode("scm9EgSU6tsqMkbs9ktwASuB-uYr17To").
+                setSystemLogin("saueihsf").
+                setSystemPasscode("scm9EgSU6tsqMkbs9ktwASuB-uYr17To").
+                setVirtualHost("saueihsf");
         config.setApplicationDestinationPrefixes("/reporteClima");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stompendpoint").withSockJS();
+        registry.addEndpoint("/stompendpoint").setAllowedOrigins("*").withSockJS();
 
     }
 }
